98690662f832693580a9abca55eeb42e57989ad3
Florencia Licarzi <37675631+florencialicarzi@users.noreply.github.com>
2021-01-20 06:45:24 -0300

    Estructura divs- Agrego boton - cambio degradado

--- README.md
+++ README.md
@@ -68,3 +68,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+    background: linear-gradient(358deg, rgba(83,201,140,1) 6%, rgba(0,0,0,1) 80%);
--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import Nav from './Components/Nav.js'
 function App() {
   return ( 
     <>
-      <Nav/>
-
-      <h1>Crea con PJLizer</h1>
-      <h2>La mejor herramienta para la creación de personajes con multiples<br/>
-          caracteristicas que te serviran de inspiracion para tu rol,<br/> historia o videojuego.
-      </h2> 
-      <h2>La mejor herramienta para la creación de personajes con multiples<br/>
-          caracteristicas que te serviran de inspiracion para tu rol,<br/> historia o videojuego.
-      </h2>
-      <h2>La mejor herramienta para la creación de personajes con multiples<br/>
-          caracteristicas que te serviran de inspiracion para tu rol,<br/> historia o videojuego.
-      </h2>
-      <h2>La mejor herramienta para la creación de personajes con multiples<br/>
-          caracteristicas que te serviran de inspiracion para tu rol,<br/> historia o videojuego.
-      </h2>
+      <div>
+          <Nav/>
+      </div>
+      <div>
+          <div className= "texto-inicio">
+              <h1>Crea con PJLizer</h1>
+              <h2>La mejor herramienta para la creación de personajes con multiples<br/>
+                  caracteristicas que te serviran de inspiracion para tu rol,<br/> 
+                  historia o videojuego.
+              </h2> 
+              
+              
+          </div>
+          <div className= "boton-inicio">
+              <button type="button" className="btn btn-light">COMIENZA YA</button>
+          </div>
+      </div>
+      
+            
 
     </>
     
